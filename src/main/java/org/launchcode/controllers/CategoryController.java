eef978d78a6138be2445b85cf1a459c65a0c95aa
Katrinka Swenson <kjswenso@gmail.com>
2019-11-04 12:09:46 -0500

    added Category template & routing

---
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -0,0 +1,48 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Category;
+import org.springframework.ui.Model;
+import org.launchcode.models.data.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        model.addAttribute("cheeses", categoryDao.findAll());
+        model.addAttribute("title", "My Cheeses Categories");
+
+        return "category/index";
+    }
+
+    @RequestMapping(value = "/add")
+    public String add(Model model) {
+        model.addAttribute(new Category());
+        model.addAttribute("title", "Add Category");
+
+        return "category/add";
+    }
+
+    @RequestMapping(value = "add")
+    public String add(Model model,
+                      @ModelAttribute @Valid Category category, Errors errors) {
+        if (errors.hasErrors()){
+            return "category/add";
+        }
+
+        categoryDao.save(category);
+
+        return "redirect:";
+    }
+}
