8d00b8b62e5606dae3ffad45f4da5f46155e8e3c
Katrinka Swenson <kjswenso@gmail.com>
2019-11-04 12:42:57 -0500

    converted enum types to category class

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,8 +1,6 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -24,7 +22,8 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+    @ManyToOne
+    private Category category;
 
     public Cheese(String name, String description) {
         this.name = name;
